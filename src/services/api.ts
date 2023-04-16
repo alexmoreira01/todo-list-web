4e0c4930688b94bc757200042ecd35a61fd2a85b
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-04-16 16:54:23 -0300

    Fix: url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: `${import.meta.env.VITE_API_URL}/todo`,
-    baseURL: `${import.meta.env.VITE_API_URL_D}/todo`
+    baseURL: `${import.meta.env.VITE_API_URL}/todo`
 });
